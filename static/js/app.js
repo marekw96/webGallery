41ef99270c42da4c438350669318614088e8cae1
Marek <marek@wiecek.pro>
2020-05-17 15:20:00 +0200

    Docker support (#1)
    Build with: docker build -t webGallery .
    Run with: docker run -b path_to_dir_on_host:/webGallery/content -p 80:5000 -d webGallery

--- static/js/app.js
+++ static/js/app.js
@@ -55,7 +55,7 @@ var app7 = new Vue({
     },
     fetchDirectory: function(url) {
       this.loading = true
-      axios.get("http://127.0.0.1:5000/getDirContent"+url)
+      axios.get("/getDirContent"+url)
         .then(response => {
           this.error = false;
           this.galleryItemList = response.data;
